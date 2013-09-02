6286073cfd142bf8c36dd08eb2698a4de9f7efe2
Sam Soffes <sam@soff.es>
2013-09-01 19:35:03 -0700

    Add `-[SAMTextView placeholderRectForBounds:]`

--- SAMTextView/SAMTextView.h
+++ SAMTextView/SAMTextView.h
@@ -15,16 +15,24 @@
 
 /**
  The string that is displayed when there is no other text in the text view.
- 
+
  The default value is `nil`.
  */
 @property (nonatomic, strong) NSString *placeholder;
 
 /**
  The color of the placeholder.
- 
+
  The default is `[UIColor lightGrayColor]`.
  */
 @property (nonatomic, strong) UIColor *placeholderTextColor;
+
+/**
+ Returns the drawing rectangle for the text views’s placeholder text.
+
+ @param bounds The bounding rectangle of the receiver.
+ @return The computed drawing rectangle for the placeholder text.
+ */
+- (CGRect)placeholderRectForBounds:(CGRect)bounds;
 
 @end
--- SAMTextView/SAMTextView.m
+++ SAMTextView/SAMTextView.m
@@ -37,7 +37,7 @@
 	if ([string isEqual:_placeholder]) {
 		return;
 	}
-	
+
 	_placeholder = string;
 	[self setNeedsDisplay];
 }
@@ -90,19 +90,34 @@
 	[super drawRect:rect];
 
 	if (self.text.length == 0 && self.placeholder) {
-		// Inset the rect
-		rect = UIEdgeInsetsInsetRect(rect, self.contentInset);
+		rect = [self placeholderRectForBounds:self.bounds];
 
-		// TODO: This is hacky. Not sure why 8 is the magic number
-		if (self.contentInset.left == 0.0f) {
-			rect.origin.x += 8.0f;
-		}
-		rect.origin.y += 8.0f;
+		UIFont *font = self.font ? self.font : self.typingAttributes[NSFontAttributeName];
 
 		// Draw the text
 		[self.placeholderTextColor set];
-		[self.placeholder drawInRect:rect withFont:self.font lineBreakMode:NSLineBreakByTruncatingTail alignment:self.textAlignment];
+		[self.placeholder drawInRect:rect withFont:font lineBreakMode:NSLineBreakByTruncatingTail alignment:self.textAlignment];
 	}
+}
+
+
+#pragma mark - Placeholder
+
+- (CGRect)placeholderRectForBounds:(CGRect)bounds {
+	// Inset the rect
+	CGRect rect = UIEdgeInsetsInsetRect(bounds, self.contentInset);
+
+	UIFont *font = self.font ? self.font : self.typingAttributes[NSFontAttributeName];
+
+	if (self.typingAttributes) {
+		NSParagraphStyle *style = self.typingAttributes[NSParagraphStyleAttributeName];
+		if (style) {
+			rect.origin.x += style.headIndent;
+			rect.origin.y += style.firstLineHeadIndent;
+		}
+	}
+
+	return rect;
 }
 
 
@@ -110,7 +125,7 @@
 
 - (void)initialize {
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textChanged:) name:UITextViewTextDidChangeNotification object:self];
-	
+
 	self.placeholderTextColor = [UIColor colorWithWhite:0.702f alpha:1.0f];
 }
 
